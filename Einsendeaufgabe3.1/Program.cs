9bc99fe086055aaf2c6c85461fda51b1202c9728
Mujo Đidić <mujocsharpdev@gmail.com>
2021-03-14 11:20:48 +0100

    Added some Comments in English Language

--- Einsendeaufgabe3.1/Program.cs
+++ Einsendeaufgabe3.1/Program.cs
@@ -14,21 +14,21 @@ namespace Einsendeaufgabe3._1
         {
             int jahresZahl;
 
-            Console.Write("Geben Sie bitte gewünschte Jahreszahl: ");
+            Console.Write("Geben Sie bitte gewünschte Jahreszahl: "); //Please enter the desired year
             jahresZahl = Convert.ToInt32(Console.ReadLine());
 
             if ((jahresZahl % 4 == 0) && (jahresZahl % 100 != 0))
             {
-                Console.WriteLine("Herzlichen glückwunsch das ist ein Schaltjahr.");
+                Console.WriteLine("Herzlichen glückwunsch das ist ein Schaltjahr.");// Herzlichen glückwunsch das ist ein Schaltjahr
 
             }
             else if ((jahresZahl % 4 == 0) && (jahresZahl % 100 == 0) && (jahresZahl % 400 == 0))
             {
-                Console.WriteLine("Herzlichen glückwunsch das ist ein Schaltjahr.");
+                Console.WriteLine("Herzlichen glückwunsch das ist ein Schaltjahr."); //Herzlichen glückwunsch das ist ein Schaltjahr
             }
             else
             {
-                Console.WriteLine("Leider das ist kein Schaltjahr.");
+                Console.WriteLine("Leider das ist kein Schaltjahr."); //Unfortunately this is not a leap year
             }
             Console.ReadLine();
         }
