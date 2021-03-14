e490f2cde7ea5fa9ad16df426648e9047e4d0f90
Mujo Đidić <mujocsharpdev@gmail.com>
2021-03-14 11:14:24 +0100

    Finished

---
+++ Einsendeaufgabe3.1/Program.cs
@@ -0,0 +1,36 @@
+﻿/*###########################################
+ * Einsendeaufgabe 3.1
+ ############################################*/
+
+
+using System;
+
+
+namespace Einsendeaufgabe3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int jahresZahl;
+
+            Console.Write("Geben Sie bitte gewünschte Jahreszahl: ");
+            jahresZahl = Convert.ToInt32(Console.ReadLine());
+
+            if ((jahresZahl % 4 == 0) && (jahresZahl % 100 != 0))
+            {
+                Console.WriteLine("Herzlichen glückwunsch das ist ein Schaltjahr.");
+
+            }
+            else if ((jahresZahl % 4 == 0) && (jahresZahl % 100 == 0) && (jahresZahl % 400 == 0))
+            {
+                Console.WriteLine("Herzlichen glückwunsch das ist ein Schaltjahr.");
+            }
+            else
+            {
+                Console.WriteLine("Leider das ist kein Schaltjahr.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
